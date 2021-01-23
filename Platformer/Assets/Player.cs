a6a45553fe23e193ddd0b8406260b571b5b8b483
nyan-code-dev <77862185+nyan-code-dev@users.noreply.github.com>
2021-01-23 14:00:27 -0600

    Made basic moving and jumping

---
+++ Platformer/Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private int jumpsLeft;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpsLeft--;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumpsLeft = 2;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        jumpsLeft = 1;
+    }
+}
